18ff97455a6a1e1ed6928685a1a59da5927bbd8f
Jagadeshwar <jagadeshwar.v@codeboardtech.com>
2024-03-14 18:11:22 +0530

    cdms prepstart 2.5

--- CDMS_DEDUP.py
+++ CDMS_DEDUP.py
@@ -24,7 +24,6 @@ import difflib
 
 # from kafka import KafkaProducer
 
-import subprocess
 
 import sys
 
@@ -553,11 +552,6 @@ def list_all_files_in_drive(drive):
 
     return all_files
 
-def remove_carriage_returns(filename):
-
-    command = ["sed", "-i", ":a;N;$!ba;s/\\r\\n//g", filename]
-
-    subprocess.run(command, check=True)
 
 config_parser = configparser.ConfigParser()
 
@@ -625,12 +619,6 @@ for file_path in files_location:
 
             df_ = pd.read_csv(i+"//"+filename,encoding='utf-16')
 
-            df_.to_csv(i.replace(CDMS_properties['replace_string'],CDMS_properties['replace_with'])+"//valid//decoded_valid_"+filename,index=False)
-
-            remove_carriage_returns(i.replace(CDMS_properties['replace_string'],CDMS_properties['replace_with'])+"//valid//decoded_valid_"+filename)
-
-            df_ = pd.read_csv(i.replace(CDMS_properties['replace_string'],CDMS_properties['replace_with'])+"//valid//decoded_valid_"+filename)
-            
             total_rows = len(df_)
 
             print('Input file Length '+'(' +filename +') '+ str(total_rows))
@@ -694,7 +682,7 @@ for file_path in files_location:
 
                 os.remove(i.replace(CDMS_properties['replace_string'],CDMS_properties['replace_with'])+"//valid//corporate_customers"+filename)
 
-            for j, chunk_df in enumerate(pd.read_csv(i.replace(CDMS_properties['replace_string'],CDMS_properties['replace_with'])+"//valid//decoded_valid_"+filename, chunksize=cunk_size, low_memory=False)):
+            for j, chunk_df in enumerate(pd.read_csv(i+"//"+filename, chunksize=cunk_size, low_memory=False,encoding='utf-16')):
 
                 # continue
 
